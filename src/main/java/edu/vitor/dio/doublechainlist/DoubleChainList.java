d4aa1893ec0291739235b62e72e1308138920a92
Vitor Lazarini <115195780+VitorVicenzo@users.noreply.github.com>
2026-07-12 13:20:37 -0300

    getNode and get method created.

--- src/main/java/edu/vitor/dio/doublechainlist/DoubleChainList.java
+++ src/main/java/edu/vitor/dio/doublechainlist/DoubleChainList.java
@@ -13,7 +13,21 @@ public class DoubleChainList<T> {
         this.listSize = 0;
     }
 
+    public T get(int index){
+        return this.getNode(index).getContent();
+    }
+
+    private DoubleNode<T> getNode(int index){
+        DoubleNode<T> auxiliarNode = firstNode;
+        for (int i = 0; (i < index) && (auxiliarNode != null); i++) {
+            auxiliarNode = auxiliarNode.getNextNode();
+        }
+        return auxiliarNode;
+    }
+
     public int size(){
         return listSize;
     }
+
+
 }
